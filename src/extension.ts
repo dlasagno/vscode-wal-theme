9b9120b5b0fa93084af1a742c2bcc7eda6c5dcaa
Daniele Lasagno <danielelasagno@gmail.com>
2019-09-07 18:59:17 +0200

    Cleaned generateColorThmes() and added a control when loading colors from pywal

--- src/extension.ts
+++ src/extension.ts
@@ -11,28 +11,29 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('walTheme.update', generateColorThemes);
 
 	context.subscriptions.push(disposable);
-	
+
 }
 
 
 function generateColorThemes() {
-	console.log('Test 1');
 	// Import colors from pywal cache
-	const colors = fs.readFileSync(path.join(os.homedir(), '/.cache/wal/colors'))
-									 .toString()
-									 .split('\n')
-									 .slice(0,16)
-									 .map(hex => Color(hex));
-	console.log(colors);
-	console.log('Test 2');
-
+	let colors: Color[];
+	try {
+		colors = fs.readFileSync(path.join(os.homedir(), '/.cache/wal/colors'))
+										 .toString()
+										 .split('\n')
+										 .slice(0,16)
+										 .map(hex => Color(hex));
+	} catch(error) {
+		vscode.window.showErrorMessage('Couldn\'t load colors from pywal cache, be sure to run pywal before updating.');
+		return;
+	}
+		
 	// Generate the normal theme
 	const colorTheme = template(colors, false);
 	fs.writeFileSync(path.join(__dirname,'../themes/wal.json'), JSON.stringify(colorTheme, null, 4));
-	console.log('Test 3');
 	
 	// Generate the bordered theme
 	const colorThemeBordered = template(colors, true);
 	fs.writeFileSync(path.join(__dirname,'../themes/wal-bordered.json'), JSON.stringify(colorTheme, null, 4));
-	console.log('Test 4');
 }
