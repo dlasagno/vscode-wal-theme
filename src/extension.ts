8b47340cbaf19e68f0c3c53e86e39eb633d967d2
Daniele Lasagno <danielelasagno@gmail.com>
2019-09-07 18:42:09 +0200

    Added the update command to refresh themes

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,38 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as os from 'os';
+import * as Color from 'color';
+import template from './template';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "wal-theme" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
+	let disposable = vscode.commands.registerCommand('walTheme.update', generateColorThemes);
 
 	context.subscriptions.push(disposable);
+	
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+
+function generateColorThemes() {
+	console.log('Test 1');
+	// Import colors from pywal cache
+	const colors = fs.readFileSync(path.join(os.homedir(), '/.cache/wal/colors'))
+									 .toString()
+									 .split('\n')
+									 .slice(0,16)
+									 .map(hex => Color(hex));
+	console.log(colors);
+	console.log('Test 2');
+
+	// Generate the normal theme
+	const colorTheme = template(colors, false);
+	fs.writeFileSync(path.join(__dirname,'../themes/wal.json'), JSON.stringify(colorTheme, null, 4));
+	console.log('Test 3');
+	
+	// Generate the bordered theme
+	const colorThemeBordered = template(colors, true);
+	fs.writeFileSync(path.join(__dirname,'../themes/wal-bordered.json'), JSON.stringify(colorTheme, null, 4));
+	console.log('Test 4');
+}
