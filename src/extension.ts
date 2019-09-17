de9b827863bb4cd4e97afcf98ef2c3ea59cef852
Daniele Lasagno <danielelasagno@gmail.com>
2019-09-17 11:58:45 +0200

    Added the auto update support and formatted

--- src/extension.ts
+++ src/extension.ts
@@ -5,21 +5,56 @@ import * as os from 'os';
 import * as Color from 'color';
 import template from './template';
 
+const walColorsPath = path.join(os.homedir(), '/.cache/wal/colors');
+let autoUpdateWatcher: fs.FSWatcher | null = null;
+
 
 export function activate(context: vscode.ExtensionContext) {
 
+	// Register the update command
 	let disposable = vscode.commands.registerCommand('walTheme.update', generateColorThemes);
-
 	context.subscriptions.push(disposable);
+
+	// Start the auto update if enabled
+	if(vscode.workspace.getConfiguration().get('walTheme.autoUpdate')) {
+		autoUpdateWatcher = autoUpdate();
+	}
+
+	// Toggle the auto update in real time when changing the extension configuration
+	vscode.workspace.onDidChangeConfiguration(event => {
+		if(event.affectsConfiguration('walTheme.autoUpdate')) {
+			if(vscode.workspace.getConfiguration().get('walTheme.autoUpdate')) {
+				if(autoUpdateWatcher === null) {
+					autoUpdateWatcher = autoUpdate();
+				}
+			}
+			else if(autoUpdateWatcher !== null) {
+				autoUpdateWatcher.close();
+				autoUpdateWatcher = null;
+			}
+		}
+	});
+
+}
+
+export function deactivate() {
+
+	// Close the watcher if active
+	if(autoUpdateWatcher !== null) {
+		autoUpdateWatcher.close();
+	}
 
 }
 
 
+/**
+ * Generates the theme from the current color palette and overwrites the last one
+ */
 function generateColorThemes() {
 	// Import colors from pywal cache
 	let colors: Color[];
 	try {
-		colors = fs.readFileSync(path.join(os.homedir(), '/.cache/wal/colors'))
+		colors = fs.readFileSync(walColorsPath)
 										 .toString()
 										 .split('\n')
 										 .slice(0,16)
@@ -36,4 +71,29 @@ function generateColorThemes() {
 	// Generate the bordered theme
 	const colorThemeBordered = template(colors, true);
 	fs.writeFileSync(path.join(__dirname,'../themes/wal-bordered.json'), JSON.stringify(colorThemeBordered, null, 4));
+}
+
+/**
+ * Automatically updates the theme when the color palette changes
+ * @returns The watcher for the color palette
+ */
+function autoUpdate(): fs.FSWatcher {
+	let fsWait = false;
+
+	// Watch for changes in the color palette of wal
+	return fs.watch(walColorsPath, (event, filename) => {
+		if (filename) {
+			// Delay after a change is found
+			if (fsWait) {
+				return;
+			}
+			fsWait = true;
+			setTimeout(() => {
+				fsWait = false;
+			}, 100);
+	
+			// Update the theme
+			generateColorThemes();
+		}
+	});
 }
